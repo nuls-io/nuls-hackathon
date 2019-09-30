2463c1c2b9dc93aa93bd92d5e00463aba86b96ef
Nancy Schorr <nms@schorrmedia.com>
2019-09-29 17:46:23 -0700

    fixing SendMailController.java

--- nuls-blockparty/src/main/java/io/nuls/controller/SendMailController.java
+++ nuls-blockparty/src/main/java/io/nuls/controller/SendMailController.java
@@ -16,17 +16,11 @@ import io.nuls.rpc.LegderTools;
 import io.nuls.rpc.TransactionTools;
 import io.nuls.rpc.vo.AccountBalance;
 import io.nuls.service.MailAddressService;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.core.MediaType;
 import java.math.BigInteger;
 import java.util.Objects;
 import java.util.Optional;
 
-
 import java.lang.System;
-//import org.javatuples.*;
 import io.nuls.base.api.provider.ServiceManager;
 import io.nuls.base.api.provider.transaction.TransferService;
 
@@ -90,11 +84,11 @@ public class SendMailController implements BaseController {
             BigInteger firstTOTALbi = costPerItemBI.multiply(itemCOUNT_BI);  // subtract from sender, add to receiver
 
             // Ennead is python-like object from org.javatuples
-            Ennead<String, String, String, Optional<MailAddressData>, Optional<MailAddressData>,
-                    byte[], byte[], BigInteger, BigInteger> mainObject_JTUP;
-
-            mainObject_JTUP = new Ennead<>(password, senderAddyStr, recMailAddyStr, senderAddy_OPT, recAddy_OPT,
-                    senderAddy_BYTES, receiverAddy_BYTES, itemCOUNT_BI, firstTOTALbi);
+//            Ennead<String, String, String, Optional<MailAddressData>, Optional<MailAddressData>,
+//                    byte[], byte[], BigInteger, BigInteger> mainObject_JTUP;
+//
+//            mainObject_JTUP = new Ennead<>(password, senderAddyStr, recMailAddyStr, senderAddy_OPT, recAddy_OPT,
+//                    senderAddy_BYTES, receiverAddy_BYTES, itemCOUNT_BI, firstTOTALbi);
 
             AccountBalance beginSenderBalance = legderTools.getBalanceAndNonce(chainId, senderAddyStr, chainId, assetId);
             System.out.println("nms senderBal before signing:  " + beginSenderBalance.getAvailable());
@@ -106,20 +100,21 @@ public class SendMailController implements BaseController {
             AccountBalance endSenderBal = legderTools.getBalanceAndNonce(chainId, senderAddyStr, chainId, assetId);
             io.nuls.controller.core.Result newResult = new Result();
             newResult.setMsg(apiResult.getMessage());
-            String s1 = "Your beginning balance was: "+beginSenderBalance + " and now it is: " + endSenderBal + "<br/>";
+
+            br =  "<br/>";
+            begBalStr = "Your beginning balance was: "  "+beginSenderBalance + ";
+            andNowBalStr = " and now it is: " + endSenderBal + br;
+
+
+            String s1 = begBalStr + " and now it is: " + endSenderBal + "<br/>";
             String s2 = "Your transfer of " + costPerItemBI +  " to blockchain address: " + recMailAddyStr +
                     "<br/>" + " was recorded in the blockchain and ledger. Reload page to do another transfer.";
             String successMsg = s1 + s2;
 
             newResult.setSuccess(apiResult.isSuccess());
             newResult.setMsg(successMsg);
-           // String theData = apiResult.getData().toString();
             newResult.setData(apiResult.getData());
             return newResult;
-
-
-
-
 
 
         });
@@ -148,129 +143,4 @@ public class SendMailController implements BaseController {
         return apiResult;
     }
 
-//    private Transaction createSendMailTransaction(SendMailReq request,
-//                                                  Ennead<String, String, String,
-//                                                          Optional<MailAddressData>, Optional<MailAddressData>,
-//                                                          byte[], byte[], BigInteger, BigInteger> myGroup)
-//            throws IOException, NulsException {
-//
-//        String senderAddy_STR = myGroup.getValue1();
-//        String receiverAddy_STR = myGroup.getValue2();
-//        Optional<MailAddressData> senderAddyData_OPT = myGroup.getValue3();
-//        Optional<MailAddressData> recAddressData_OPT = myGroup.getValue4();
-//        byte[] senderAddressBytes = myGroup.getValue5();
-//        byte[] receiverAddressBytes = myGroup.getValue6();
-//
-//        if (senderAddyData_OPT.isEmpty()) {
-//            throw new NulsRuntimeException(CommonCodeConstanst.PARAMETER_ERROR, "can't find sender address: " + senderAddy_STR);
-//        }
-//        if (recAddressData_OPT.isEmpty()) {
-//            throw new NulsRuntimeException(CommonCodeConstanst.PARAMETER_ERROR, "can't find receiver address: " + receiverAddy_STR);
-//        }
-//        if (Arrays.equals(receiverAddressBytes, senderAddressBytes)) {
-//            throw new NulsRuntimeException(CommonCodeConstanst.PARAMETER_ERROR, "sender equals receiver");
-//        }
-//        Account account = new Account();
-//
-//        Transaction tx = new Transaction();
-//        String a = account.getEncryptedPrikeyHex();
-//        String h = account.getPubkeyHex();
-//
-//        tx.setType(Constant.TX_TYPE_SEND_MAIL);
-//        tx.setTime(NulsDateUtils.getCurrentTimeSeconds());
-//
-//        byte[] txData = buildTxData(myGroup);
-//        byte[] cd = buildCoinData(tx, myGroup);
-//
-//        tx.setTxData(txData);
-//        tx.setCoinData(cd);
-//
-//        System.out.println("nms transaction signature in Transaction(): " +
-//                Arrays.toString(tx.getTransactionSignature()));
-//
-//        return tx;
-//    }
-
-
-//    private byte[] buildTxData(Ennead<String, String, String,
-//            Optional<MailAddressData>, Optional<MailAddressData>,
-//            byte[], byte[],
-//            BigInteger, BigInteger> myGroup) throws IOException {
-//
-//        Optional<MailAddressData> senderAddyData_OPT = myGroup.getValue3();
-//        Optional<MailAddressData> recAddressData_OPT = myGroup.getValue4();
-//        byte[] senderAddress_BTS = myGroup.getValue5();
-//        byte[] receiverAddress_BTS = myGroup.getValue6();
-//        MailAddressData sendAdd_DATA = senderAddyData_OPT.get();
-//        if (senderAddyData_OPT.isEmpty()) {
-//            throw new NulsRuntimeException(CommonCodeConstanst.PARAMETER_ERROR, "can't find sender address: " + senderAddyData_OPT);
-//        }
-//        MailAddressData recAdd_DATA = recAddressData_OPT.get();
-//        String senderPubKey_STR = sendAdd_DATA.getPubKey();
-//        String recPubKey_STR = recAdd_DATA.getPubKey();
-//
-//        ECKey key = new ECKey();
-//        byte[] pubkey = key.getPubKey();
-//        byte[] privKey_BYTES = key.getPrivKeyBytes();
-//
-//        byte[] sender_ECK = ECIESUtil.encrypt(HexUtil.decode(senderPubKey_STR), privKey_BYTES);
-//        byte[] receiver_ECK = ECIESUtil.encrypt(HexUtil.decode(recPubKey_STR), privKey_BYTES);
-//        byte[] daysContent_BYTES = myGroup.getValue7().toString().getBytes(StandardCharsets.UTF_8);
-//        byte[] countTitle_BYTES = myGroup.getValue8().toString().getBytes(StandardCharsets.UTF_8);
-//
-//        MailContent content = new MailContent();
-//        content.setContent(ECIESUtil.encrypt(pubkey, daysContent_BYTES));
-//        content.setTitle(ECIESUtil.encrypt(pubkey, countTitle_BYTES));
-//        content.setReceiverAddress(receiverAddress_BTS);
-//        content.setReceiverKey(receiver_ECK);
-//        content.setSenderAddress(senderAddress_BTS);
-//        content.setSenderKey(sender_ECK);
-//        content.setTimestamp(System.currentTimeMillis());
-//        return content.serialize();
-//    }
-//
-//
-//    private byte[] buildCoinData(Transaction tx,
-//                                 Ennead<String, String, String, Optional<MailAddressData>, Optional<MailAddressData>,
-//                                         byte[], byte[], BigInteger, BigInteger> myGroup)
-//            throws IOException, NulsException {
-//        int chId = config.getChainId();
-//        int asId = config.getAssetId();
-//        String sendAddyStr = myGroup.getValue1();
-//        byte[] senderAddressBytes = myGroup.getValue5();
-//        byte[] receiverAddressBytes = myGroup.getValue6();
-//        BigInteger firstCost = myGroup.getValue8();
-//        byte locked = 0;
-//
-//        AccountBalance sendAcctBal_OBJ = legderTools.getBalanceAndNonce(chId, sendAddyStr, chId, asId);
-//        BigInteger avail_BI = sendAcctBal_OBJ.getAvailable();
-//        byte[] nonce = RPCUtil.decode(sendAcctBal_OBJ.getNonce());
-//        CoinFrom coinFrom = new CoinFrom();
-//
-//        coinFrom.setAddress(senderAddressBytes);
-//        coinFrom.setAssetsChainId(chId);
-//        coinFrom.setAssetsId(asId);
-//        coinFrom.setAmount(firstCost);
-//        coinFrom.setLocked(locked);
-//        coinFrom.setNonce(nonce);
-//
-//        CoinTo coinTo = new CoinTo(receiverAddressBytes, chId, asId, firstCost);
-//        coinTo.setAddress(receiverAddressBytes);
-//
-//        int txSize = tx.size() + coinFrom.size() + coinTo.size() + P2PHKSignature.SERIALIZE_LENGTH;
-//        BigInteger sizeBasedTxFee_BI = TransactionFeeCalculator.getNormalTxFee(txSize);
-//        BigInteger totalCostPlusFee_BI = firstCost.add(sizeBasedTxFee_BI);
-//
-//        if (avail_BI.min(totalCostPlusFee_BI).equals(avail_BI)) {
-//            throw new NulsRuntimeException(CommonCodeConstanst.FAILED, "insufficient fee");
-//        }
-//        coinFrom.setAmount(totalCostPlusFee_BI);
-//
-//        CoinData coinData = new CoinData();
-//        coinData.setFrom(List.of(coinFrom));
-//        coinData.setTo(List.of(coinTo));
-//
-//        return coinData.serialize();
-//    }
-//
 }
